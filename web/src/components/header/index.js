207f10b6de3b61f813c67afca4bee2c90d867675
G-Basilio <gabrielmrsoli@hotmail.com>
2022-04-25 23:38:56 -0300

    tela inicial

---
+++ web/src/components/header/index.js
@@ -0,0 +1,11 @@
+import Logo from "../../assets/logo-white.png";
+
+const Header = () => {
+  return (
+    <div className="col-12">
+      <img src={Logo} />
+    </div>
+  );
+};
+
+export default Header;
