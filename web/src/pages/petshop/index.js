d2335f19ea52da53cb19ffa73d087c4d7072c7bc
G-Basilio <gabrielmrsoli@hotmail.com>
2022-04-29 22:05:39 -0300

    adicionando um pagina card

--- web/src/pages/petshop/index.js
+++ web/src/pages/petshop/index.js
@@ -1,4 +1,5 @@
 import Header from "../../components/header";
+import "./styles.css";
 
 const Petshop = () => {
   return (
@@ -7,13 +8,24 @@ const Petshop = () => {
       <div className="container">
         <div className="row">
           <div className="col-2">
-              <img src="https://www.petlove.com.br/static/uploads/banner_image/image/4304/logo-petlove-push.png"
-              className="img-fluid"
-              />
-              <b>Petlove</b>
-              PAREI COM 1:10:28
+            <img
+              src="https://www.petlove.com.br/static/uploads/banner_image/image/4304/logo-petlove-push.png"
+              className="img-fluid petshop-images"
+            />
+            <b>Petlove</b>
+            <div className="petshop-infos">
+              <span className="mdi mdi-star"></span>
+              <text>
+                <b>4,2</b>
+              </text>
+              <span className="mdi mdi-cash-usd-outline"></span>
+              <text>$$$</text>
+              <span className="mdi mdi-crosshairs-gps"></span>
+              <text>3,9km</text>
+            </div>
+            <label className="badge badge-primary">Frete Grátis </label>
           </div>
-            <div className="col-10"></div>
+          <div className="col-10"></div>
         </div>
       </div>
     </div>
