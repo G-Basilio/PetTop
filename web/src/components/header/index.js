9df9c21b812f239c8338558477282adf21515182
G-Basilio <gabrielmrsoli@hotmail.com>
2022-05-05 20:36:45 -0300

    adicionando Sidebar

--- web/src/components/header/index.js
+++ web/src/components/header/index.js
@@ -1,12 +1,28 @@
 import Logo from "../../assets/logo-white.png";
 import LogoWhite from '../../assets/logo.png';
 
+import './styles.css';
+
 const Header = ({ whiteVersion }) => {
+
+
+  const openDrawer = () => {
+    const event = new CustomEvent('openCart');
+    window.dispatchEvent(event);
+  }
+
+
   return (
     <div className="col-12">
     <header className="py-4px text-center">
       <img src={ whiteVersion ?LogoWhite : Logo } className="img-fluid" />
     </header>
+    <button 
+    onClick={() => openDrawer}
+    className="btn btn-secondary cart-button"
+    >
+      <spam className="mdi mdi-cart"></spam> 2 ítens
+      </button>
     </div>
   );
 };
--- web/src/components/product/card/index.js
+++ web/src/components/product/card/index.js
@@ -1,1 +1,21 @@
-//1:22:51
+import './styles.css';
+
+const Product = () => {
+    return (
+        <div className="product col-3">
+            <img 
+            src="https://guabinatural.com.br/wp-content/uploads/2020/09/GN-Cao-Filhote-Mini-e-Pequeno-Frango-e-Arroz-Integral-1kg-FRENTE.png"
+            className="img-fluid align-center"
+            />
+            <button className="btn btn-primary rounded-circle">+</button>
+            <h4>
+                <label className="badge badge-primary">R$ 100,00</label>
+            </h4>
+            <small>
+                <b>Ração Guabi Natural Adulto Raças Pequenas e Mini</b>
+            </small>
+        </div>
+    );
+};
+
+export default Product;
